ea904bd26e5987a3d6b7a358af1605c4ad051573
NTMDev <xinanli360@gmail.com>
2026-07-22 19:13:07 -0400

    Release 1.7.4 BugFix (Patch #43)

--- source/PJDK.py
+++ source/PJDK.py
@@ -3516,9 +3516,11 @@ class Method:
 
                 print('[OUTPUT]: ' + str(value), end=endChar)
                 self.tokPosition += len(expr_tokens) + 1  
-
+                if self.lang[self.tokPosition+1].get()['type'] != 'SEMICOLON':
+                    raise EOFError('Unexpected reached end of line, when expected semicolon')
                 if self.tokPosition < len(self.lang) and self.lang[self.tokPosition].get()['type'] == 'SEMICOLON':
                     self.tokPosition += 1
+                
             
         self.tokPosition += 1
         return False
